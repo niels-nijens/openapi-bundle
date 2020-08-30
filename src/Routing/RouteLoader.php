34af1b1d1d55bb998a60b449ef116efedb1f76c2
Niels Nijens <nijens.niels@gmail.com>
2020-08-30 22:40:38 +0200

    Add RouteLoader implementation compatible with Symfony 5.x

--- src/Routing/RouteLoader.php
+++ src/Routing/RouteLoader.php
@@ -4,26 +4,54 @@ declare(strict_types=1);
 
 namespace Nijens\OpenapiBundle\Routing;
 
-/**
- * {@see AbstractRouteLoader} implementation for Symfony versions 3.4 and 4.4.
- *
- * @author Niels Nijens <nijens.niels@gmail.com>
- */
-class RouteLoader extends AbstractRouteLoader
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function supports($resource, $type = null)
-    {
-        return parent::doSupports($resource, $type);
-    }
+use Symfony\Component\HttpKernel\Kernel;
 
+if (Kernel::MAJOR_VERSION < 5) {
     /**
-     * {@inheritdoc}
+     * {@see AbstractRouteLoader} implementation for Symfony versions 3.4 and 4.4.
+     *
+     * @author Niels Nijens <nijens.niels@gmail.com>
      */
-    public function load($resource, $type = null)
+    class RouteLoader extends AbstractRouteLoader
     {
-        return parent::doLoad($resource, $type);
+        /**
+         * {@inheritdoc}
+         */
+        public function supports($resource, $type = null)
+        {
+            return parent::doSupports($resource, $type);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function load($resource, $type = null)
+        {
+            return parent::doLoad($resource, $type);
+        }
+    }
+} else {
+    /**
+     * {@see AbstractRouteLoader} implementation for Symfony version 5.x.
+     *
+     * @author Niels Nijens <nijens.niels@gmail.com>
+     */
+    class RouteLoader extends AbstractRouteLoader
+    {
+        /**
+         * {@inheritdoc}
+         */
+        public function supports($resource, string $type = null)
+        {
+            return parent::doSupports($resource, $type);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function load($resource, string $type = null)
+        {
+            return parent::doLoad($resource, $type);
+        }
     }
 }
